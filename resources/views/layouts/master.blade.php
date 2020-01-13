4ceeeb54149e67654c9c540a86c7c5d863b44773
lypsisrudiansyah <rudiansyahether@gmail.com>
2020-01-13 16:03:34 +0700

    add: passport to this project

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -108,14 +108,23 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
         <li class="nav-item">
-            <router-link to="/profile" class="nav-link">
-                <i class="nav-icon fas fa-user"></i>
+            <router-link to="/developer" class="nav-link">
+                <i class="nav-icon fas fa-cogs"></i>
                 <p>
-                    Profile
-                    <span class="right badge badge-danger">New</span>
+                    Developer
                 </p>
             </router-link>
         </li>
+
+        <li class="nav-item">
+          <router-link to="/profile" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                  Profile
+                  <span class="right badge badge-danger">New</span>
+              </p>
+          </router-link>
+      </li>
 
         <li class="nav-item">
             <a class="nav-link" href="{{ route('logout') }}"
